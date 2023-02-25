01483f16f322ac75d8269691466dc15e7bf4a1ed
Chris Noreikis <cnoreikis@spins.com>
2023-02-25 13:28:24 -0600

    dynamic programming solution

--- src/test/java/algorithms/EditDistanceTest.java
+++ src/test/java/algorithms/EditDistanceTest.java
@@ -9,5 +9,20 @@ public class EditDistanceTest {
     public void testOne() {
         EditDistance uut = new EditDistance();
         assertEquals(1, uut.editDistanceIntuitive("ab", "abc"));
+        assertEquals(1, uut.editDistanceDynamicProgramming("ab", "abc"));
+    }
+
+    @Test
+    public void testTwo() {
+        EditDistance uut = new EditDistance();
+        assertEquals(3, uut.editDistanceIntuitive("horse", "ros"));
+        assertEquals(3, uut.editDistanceDynamicProgramming("horse", "ros"));
+    }
+
+    @Test
+    public void testThree() {
+        EditDistance uut = new EditDistance();
+        assertEquals(5, uut.editDistanceIntuitive("intention", "execution"));
+        assertEquals(5, uut.editDistanceDynamicProgramming("intention", "execution"));
     }
 }
