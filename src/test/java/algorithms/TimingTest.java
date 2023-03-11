e0b0953acd97d9f2853ce5cc5651c82a75fdbfc5
Chris Noreikis <chris.nore@gmail.com>
2023-03-11 08:59:28 -0600

    fix timing tests, initial and target were being reset to strings of length 1 and 2 on for every trial run so the last "+=" statement had no effect

--- src/test/java/algorithms/TimingTest.java
+++ src/test/java/algorithms/TimingTest.java
@@ -1,53 +1,66 @@
 package algorithms;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import tester.timing.TimingHelper;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
 public class TimingTest {
-	EditDistanceAlgorithm recursiveImpl;
-	EditDistanceAlgorithm intuitiveImpl;
-	EditDistanceAlgorithm dynamicImpl;
-	TimingHelper intuitiveTiming = new TimingHelper();
-	TimingHelper recursiveTiming = new TimingHelper();
-	TimingHelper dynamicTiming = new TimingHelper();
-	String initial;
-	String target;
-	
-	@Before
-	public void setUp() throws Exception {
-		recursiveImpl = new RecursiveImpl();
-		intuitiveImpl = new IntuitiveImpl();
-		dynamicImpl = new DynamicImpl();
-	}
+    EditDistanceAlgorithm recursiveImpl;
+    EditDistanceAlgorithm intuitiveImpl;
+    EditDistanceAlgorithm dynamicImpl;
+    TimingHelper intuitiveTiming = new TimingHelper();
+    TimingHelper recursiveTiming = new TimingHelper();
+    TimingHelper dynamicTiming = new TimingHelper();
+    @Before
+    public void setUp() throws Exception {
+        recursiveImpl = new RecursiveImpl();
+        intuitiveImpl = new IntuitiveImpl();
+        dynamicImpl = new DynamicImpl();
+    }
 
-	@Test
-	public void testTiming() {
-		int index = 1;
-		while (index <= 20000) {
-			initial = "a";
-			target = "ab";
-			intuitiveTiming.start();			
-			int initAnswer = intuitiveImpl.solve(initial, target);
-			intuitiveTiming.stop();
-			recursiveTiming.start();
-			int recrAnswer = recursiveImpl.solve(initial, target);
-			recursiveTiming.stop();
-			dynamicTiming.start();
-			int dynAnswer = dynamicImpl.solve(initial, target);
-			dynamicTiming.stop();
-			assertEquals(initAnswer, recrAnswer);
-			assertEquals(initAnswer, dynAnswer);
-			index++;
-			initial += "a";
-			target += "a";
-		}
-		System.out.println("Intuitive: " + intuitiveTiming.getOverview());
-		System.out.println("Recursive: " + recursiveTiming.getOverview());
-		System.out.println("Dynamic: " + dynamicTiming.getOverview());
-	}
+    @Test
+    public void testTiming() {
+        int index = 1;
+        int numTrials = 500;
+        while (index <= numTrials) {
+            int randomStrLen = new Random().nextInt(10);
+            String initial = randomStr(randomStrLen);
+            String target = randomStr(randomStrLen);
+            System.out.println("initial = " + initial);
+            System.out.println("target = " + target);
+            System.out.println("trial number = " + index);
+
+            intuitiveTiming.start();
+            int initAnswer = intuitiveImpl.solve(initial, target);
+            intuitiveTiming.stop();
+            recursiveTiming.start();
+            int recrAnswer = recursiveImpl.solve(initial, target);
+            recursiveTiming.stop();
+            dynamicTiming.start();
+            int dynAnswer = dynamicImpl.solve(initial, target);
+            dynamicTiming.stop();
+            assertEquals(initAnswer, recrAnswer);
+            assertEquals(initAnswer, dynAnswer);
+            index++;
+        }
+        System.out.println("Intuitive: " + intuitiveTiming.getOverview());
+        System.out.println("Recursive: " + recursiveTiming.getOverview());
+        System.out.println("Dynamic: " + dynamicTiming.getOverview());
+    }
+
+    private String randomStr(int len) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            s.append(randomChar());
+        }
+        return s.toString();
+    }
+
+    private char randomChar() {
+        return (char)(new Random().nextInt(26) + 'a');
+    }
 }
